425f8b5e2cbe5187ec1e0654ff4c8170a6e2cad4
apehex <17317183+apehex@users.noreply.github.com>
2026-04-30 15:51:12 +0200

    Define the training callbacks.

---
+++ src/deformers/pipelines/train.py
@@ -0,0 +1,148 @@
+import os.path
+
+import torch
+import torch.utils.tensorboard
+import tqdm
+
+import deformers.pipelines.monitor
+
+# GENERIC ######################################################################
+
+def noop(*args, **kwargs) -> None:
+    pass
+
+# CHECK ########################################################################
+
+def is_callback(callback: dict) -> bool:
+    return (
+        all(__k in callback for __k in ['name', 'trigger', 'operation', 'cleanup'])
+        and isinstance(callback['name'], str)
+        and callable(callback['trigger'])
+        and callable(callback['operation'])
+        and callable(callback['cleanup']))
+
+# FORMAT #######################################################################
+
+def format_state(state: dict) -> dict:
+    """Group and format the state variables to export them."""
+    return {
+        'switch': f"[{' '.join(state['switch/train'] * ['train'] + (not state['switch/train']) * ['test'] + state['switch/grad'] * ['grad'] + state['switch/log'] * ['log'] + state['switch/save'] * ['save'])}]",
+        'epoch': f"({state['epoch/current']}/{state['epoch/total']})",
+        'step': f"({state['step/current']}/{state['step/total']})",
+        'loss': f"(ema: {state['loss/ema']:.6f} total: {state['loss/total']:.6f} mse(0: {state['loss/mse/0']:.6f} k: {state['loss/mse/k']:.6f}) cos(0: {state['loss/cos/0']:.6f} k: {state['loss/cos/k']:.6f}))",
+        'gradient': f"(rate: {state['gradient/rate']:.2e} norm: {state['gradient/norm']:.4f})",
+        'iter': f"(time: {state['iter/time'] * 1000.0:.0f} tok/s: {state['iter/tps']:.0f})",
+        'vocab': f"(seen: {state['vocab/seen'] * 100.0:.1f}% min: {state['vocab/min']} max: {state['vocab/max']})",}
+
+# LOGGING ######################################################################
+
+def prepare_logging_callback(
+    step_num: int,
+    path_str: str,
+) -> dict:
+    # create the parent directory
+    os.makedirs(os.path.dirname(path_str), exist_ok=True)
+    # open the file
+    __file = open(path_str, 'w')
+    # test whether the callback should be run
+    def __trigger(state: dict) -> bool:
+        return (state['step/current'] % step_num) == 0
+    # write the state to the target file
+    def __operation(state: dict) -> None:
+        # aggregate and format
+        __stats = format_state(state=state)
+        # write as a single line
+        __file.write(deformers.pipelines.monitor.serialize_state(state=__stats, prefix='') + '\n')
+    # close the file on cleanup
+    def __cleanup() -> None:
+        __file.close()
+    # format as a callback
+    return {
+        'name': 'log',
+        'trigger': __trigger,
+        'operation': __operation,
+        'cleanup': __cleanup,}
+
+def prepare_tensorboard_callback(
+    step_num: int,
+    path_str: str,
+) -> dict:
+    # create the parent directory
+    os.makedirs(path_str, exist_ok=True)
+    # open the file
+    __writer = torch.utils.tensorboard.SummaryWriter(log_dir=path_str)
+    # test whether the callback should be run
+    def __trigger(state: dict) -> bool:
+        return (state['step/current'] % step_num) == 0
+    # write the state to the target file
+    def __operation(state: dict) -> None:
+        # filter out the tensors
+        __state = {__k: __v for (__k, __v) in state.items() if not 'inputs' in __k}
+        # write all the scalars
+        deformers.pipelines.monitor.log_scalars(writer=__writer, step=state['step/current'], scalars=__state)
+    # close the file on cleanup
+    def __cleanup() -> None:
+        __writer.close()
+    # format as a callback
+    return {
+        'name': 'log',
+        'trigger': __trigger,
+        'operation': __operation,
+        'cleanup': __cleanup,}
+
+# PROGRESS #####################################################################
+
+def prepare_progress_callback(
+    step_num: int,
+    epoch_num: int,
+    epoch_tot: int,
+    dataset_obj: object,
+) -> dict:
+    # create a progress bar
+    __pbar = tqdm.tqdm(
+        dataset_obj,
+        total=len(dataset_obj),
+        desc=f'epoch {epoch_num}/{epoch_tot}',
+        unit='batch',
+        leave=True)
+    # test whether the callback should be run
+    def __trigger(state: dict) -> bool:
+        return (state['step/current'] % step_num) == 0
+    # write the state to the target file
+    def __operation(state: dict) -> None:
+        # aggregate and format
+        __stats = format_state(state=state)
+        # filter the epoch and step since they are already in the pbar
+        __pbar.set_postfix({__k: __v for (__k, __v) in __stats.items() if (__k not in ['epoch', 'step'])})
+    # close the file on cleanup
+    def __cleanup() -> None:
+        __pbar.close()
+    # format as a callback
+    return {
+        'name': 'pbar',
+        'trigger': __trigger,
+        'operation': __operation,
+        'cleanup': __cleanup,}
+
+# CHECKPOINT ###################################################################
+
+def prepare_checkpoint_callback(
+    step_num: int,
+    path_str: str,
+    model_obj: object,
+) -> dict:
+    # create the parent directory
+    os.makedirs(os.path.dirname(path_str), exist_ok=True)
+    # test whether the callback should be run
+    def __trigger(state: dict) -> bool:
+        return (state['step/current'] % step_num) == 0
+    # write the state to the target file
+    def __operation(state: dict) -> None:
+        # save the configuration and state of the model in a single file
+        model_obj.save_checkpoint(path=path_str)
+    # format as a callback
+    return {
+        'name': 'save',
+        'trigger': __trigger,
+        'operation': __operation,
+        'cleanup': noop,}
