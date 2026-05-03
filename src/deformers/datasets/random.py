a1d524d2c9e54d7d53c03439d838a12742c5d284
apehex <17317183+apehex@users.noreply.github.com>
2026-05-03 18:39:27 +0200

    Generator full tensors instead of row samples.

--- src/deformers/datasets/random.py
+++ src/deformers/datasets/random.py
@@ -1,23 +1,31 @@
 import datasets
-import random
+import torch
 
 # UNIFORM ######################################################################
 
 def _uniform_generator(
     dataset_len: int,
     vocab_dim: int,
+    batch_dim: int,
     sequence_dim: int,
     seed_num: int=1337,
 ) -> iter:
-    __gen = random.Random(seed_num)
+    __gen = torch.Generator()
+    __gen.manual_seed(seed_num)
     # number of samples
     for _ in range(dataset_len):
         # dictionary of columns
-        yield {'indices': [__gen.randrange(vocab_dim) for _ in range(sequence_dim)],}
+        yield {
+            'indices': torch.randint(
+                low=0,
+                high=vocab_dim,
+                size=(batch_dim, sequence_dim),
+                generator=__gen),}
 
 def build_uniform_dataset(
     dataset_len: int,
     vocab_dim: int,
+    batch_dim: int,
     sequence_dim: int,
     seed_num: int=1337,
 ) -> datasets.Dataset:
@@ -26,5 +34,6 @@ def build_uniform_dataset(
         gen_kwargs={
             'dataset_len': dataset_len,
             'vocab_dim': vocab_dim,
+            'batch_dim': batch_dim,
             'sequence_dim': sequence_dim,
             'seed_num': seed_num,})
