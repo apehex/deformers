28386cfc7986c46dbc0f372e0cc04a62becc9900
apehex <17317183+apehex@users.noreply.github.com>
2026-05-26 19:37:05 +0200

    Account for the limit in the size calculation.

--- src/deformers/datasets/generic.py
+++ src/deformers/datasets/generic.py
@@ -5,15 +5,16 @@ import itertools
 class BatchedDataset:
     """Provide a length to datasets made from an iterator."""
 
-    def __init__(self, dataset_obj: object, batch_dim: int, batch_num: int=None) -> None:
+    def __init__(self, dataset_obj: object, batch_dim: int, batch_num: int=-1) -> None:
         self._dataset = dataset_obj
         self._batch = int(batch_dim)
-        self._count = batch_num
+        self._count = int(batch_num)
 
     def __len__(self) -> int:
-        return len(self._dataset) // max(1, (self._batch))
+        __size = len(self._dataset) // max(1, (self._batch))
+        return __size if (self._count < 1) else min(__size, self._count)
 
     def __iter__(self) -> object:
         return itertools.islice(
             self._dataset.iter(batch_size=self._batch),
-            self._count)
+            None if (self._count < 1) else self._count)
