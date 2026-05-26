375dad0d1fafb1ea22dd94c625ef1c4c5cd727c9
apehex <17317183+apehex@users.noreply.github.com>
2026-05-26 16:50:56 +0200

    Add an optional limit to the number of batches.

--- src/deformers/datasets/generic.py
+++ src/deformers/datasets/generic.py
@@ -1,14 +1,19 @@
+import itertools
+
 # WRAPPER ######################################################################
 
 class BatchedDataset:
     """Provide a length to datasets made from an iterator."""
 
-    def __init__(self, dataset_obj: object, batch_dim: int) -> None:
+    def __init__(self, dataset_obj: object, batch_dim: int, batch_num: int=None) -> None:
         self._dataset = dataset_obj
         self._batch = int(batch_dim)
+        self._count = batch_num
 
     def __len__(self) -> int:
         return len(self._dataset) // max(1, (self._batch))
 
     def __iter__(self) -> object:
-        return self._dataset.iter(batch_size=self._batch)
+        return itertools.islice(
+            self._dataset.iter(batch_size=self._batch),
+            self._count)
